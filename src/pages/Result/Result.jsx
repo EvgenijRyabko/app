e77cfab81044be8f4e923226a49ce09851a85415
Firstredby <imshchirov@gmail.com>
2023-06-10 16:35:37 +0300

    Result upd

--- src/pages/Result/Result.jsx
+++ src/pages/Result/Result.jsx
@@ -57,13 +57,25 @@ function Result({ answers }) {
 
 					switch (res?.name) {
 						case "eng":
-							return <div>Engineer</div>;
+							return <div>Engineer
+								{/* https://img.freepik.com/free-vector/science-concept-scientist-in-laboratory-experiment-research-biology-chemistry-physics-knowledge_513217-121.jpg?w=1800&t=st=1686354429~exp=1686355029~hmac=6e94ff6d295b34ee0cf81dee4b256b8088eff3434afbfd230b80295cfb1e2521 */
+								/* https://img.freepik.com/free-vector/science-concept-scientist-in-laboratory-experiment-research-biology-chemistry-physics-knowledge_513217-121.jpg?w=1800&t=st=1686354429~exp=1686355029~hmac=6e94ff6d295b34ee0cf81dee4b256b8088eff3434afbfd230b80295cfb1e2521 */}
+							</div>;
 						case "prog":
-							return <div>Programmer</div>;
+							return <div>Programmer
+								{/* https://img.freepik.com/free-vector/programmer-concept-illustration_114360-2417.jpg?w=1380&t=st=1686354621~exp=1686355221~hmac=7c5a619ec766fa889850cb65371ea8a8ea7c5c8e25682d159d9ec2d994d4b07e
+								 https://img.freepik.com/free-vector/binary-code-white-background-with-floating-numbers_1017-25331.jpg?w=1800&t=st=1686354673~exp=1686355273~hmac=0df5a897f643dda31f6aef5d772b30b6b5fb7eb38efcfb4b8bad63f70e830b93*/}
+							</div>;
 						case "hum":
-							return <div>Hummanitarian</div>;
+							return <div>Hummanitarian
+								{/* https://img.freepik.com/free-vector/tiny-screenwriter-sitting-on-retro-typewriter-thinking-screenplay-while-paper-drafts-flying-around-author_74855-14178.jpg?w=1800&t=st=1686354809~exp=1686355409~hmac=a6726f089f058447daed5413f6cc466217657a76fcc7554f0880027c718a8ce9
+								 https://img.freepik.com/premium-vector/vector-line-seamless-pattern-books-with-bookmark-open-book_439591-1425.jpg?w=1380*/}
+							</div>;
 						case "art":
-							return <div>Clown</div>;
+							return <div>Artist
+								{/* https://img.freepik.com/free-vector/smiling-woman-standing-near-easel-and-painting-flat-illustration_74855-11057.jpg?w=1800&t=st=1686354901~exp=1686355501~hmac=e82f48bdd337ddab8eb093bd4c973cda1306817671fb453b8620cda1dc57bbac
+								 https://img.freepik.com/premium-vector/colorful-paint-splatter-background-rainbow-splashes-abstract-color-spray-explosion-vector-banner_461812-693.jpg?w=1800*/}
+							</div>;
 						default:
 							return <></>
 					}
