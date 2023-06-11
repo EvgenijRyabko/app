44a8443435dc499085d5ca348d34956585b7d4d1
Firstredby <imshchirov@gmail.com>
2023-06-11 07:34:31 +0300

    add flags

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,21 @@ import Greeting from '../pages/Greeting/Greeting';
 import MainQuiz from '../pages/MainQuiz/MainQuiz';
 import Result from '../pages/Result/Result';
 
+
 function App() {
 	const [answers, setAnswers] = useState([]);
 	const [lang, setLang] = useState('uk');
 
 	return (
 		<BrowserRouter>
-			<select className="absolute bg-transparent min-w-[100px] z-[9999] left-0 bottom-0" onChange={(e) => setLang(e.target.value)}>
-				<option value={'uk'}>uk</option>
-				<option value={'ru'}>ru</option>
-				<option value={'en'}>en</option>
-			</select>
+			<div className="absolute bg-transparent min-w-[120px] z-[9999] left-2 bottom-1" onChange={(e) => setLang(e.target.value)}>
+				<select className='bg-transparent w-[90px]'>
+					<option value={'uk'}>🇺🇦</option>
+					<option value={'en'}>🇬🇧</option>
+					<option value={'ru'}>🇷🇺</option>
+				</select>
+			</div>
+			
 			<Routes>
 				<Route path="/" element={<Greeting lang={lang} />} />
 				<Route path="/quiz" element={<MainQuiz lang={lang} answers={answers} setAnswers={setAnswers} />} />
